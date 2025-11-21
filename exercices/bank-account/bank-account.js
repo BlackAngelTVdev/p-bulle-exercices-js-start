ca995e108e444b9a7b50509d6c1dc08e94ea47ae
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2025-11-21 14:30:40 +0100

    Feat(bank) : finished
    [DONE]

--- exercices/bank-account/bank-account.js
+++ exercices/bank-account/bank-account.js
@@ -1,36 +1,52 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export class BankAccount {
-  constructor() {
-    throw new Error('Remove this line and implement the function');
-  }
-
-  open() {
-    throw new Error('Remove this line and implement the function');
-  }
-
-  close() {
-    throw new Error('Remove this line and implement the function');
-  }
-
-  deposit() {
-    throw new Error('Remove this line and implement the function');
-  }
-
-  withdraw() {
-    throw new Error('Remove this line and implement the function');
-  }
-
-  get balance() {
-    throw new Error('Remove this line and implement the function');
-  }
-}
-
 export class ValueError extends Error {
   constructor() {
     super('Bank account error');
+  }
+}
+
+export class BankAccount {
+  #balance;
+  #isOpen;
+
+  constructor() {
+    this.#balance = 0;
+    this.#isOpen = false;
+  }
+
+  open() {
+    if (this.#isOpen) {
+      throw new ValueError();
+    }
+    this.#isOpen = true;
+    this.#balance = 0;
+  }
+
+  close() {
+    if (!this.#isOpen) {
+      throw new ValueError();
+    }
+    this.#isOpen = false;
+    this.#balance = 0;
+  }
+
+  deposit(amount) {
+    if (!this.#isOpen || amount <= 0) {
+      throw new ValueError();
+    }
+    this.#balance += amount;
+  }
+
+  withdraw(amount) {
+    if (!this.#isOpen || amount <= 0 || this.#balance < amount) {
+      throw new ValueError();
+    }
+    this.#balance -= amount;
+  }
+
+  get balance() {
+    if (!this.#isOpen) {
+      throw new ValueError();
+    }
+    return this.#balance;
   }
 }
