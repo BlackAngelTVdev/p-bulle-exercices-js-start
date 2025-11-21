1f1bd3ef8b631aee443b2a1142c7ef039a66407a
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2025-11-21 14:16:06 +0100

    Feat(Space age) : finished 🔥
    [DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+const SECONDS_PER_EARTH_YEAR = 31557600;
+
+const ORBITAL_PERIODS = {
+  earth: 1.0,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+export const age = (planet, seconds) => {
+  const orbitalPeriod = ORBITAL_PERIODS[planet];
+
+  if (!orbitalPeriod) {
+    throw new Error("not a planet"); 
+  }
+
+  const ageInEarthYears = seconds / SECONDS_PER_EARTH_YEAR;
+
+  const ageOnPlanet = ageInEarthYears / orbitalPeriod;
+
+  return Math.round(ageOnPlanet * 100) / 100;
 };
