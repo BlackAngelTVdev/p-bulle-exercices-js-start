96a063b0500bb7f3d117bd21fbc585e2ed50645f
Damien Rochat <dami.scoot3@gmail.com>
2025-12-19 15:52:59 +0100

    Feat(/exercices/linked-list/): finished

--- exercices/linked-list/linked-list.js
+++ exercices/linked-list/linked-list.js
@@ -2,29 +2,102 @@
 // This is only a SKELETON file for the 'Linked List' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// @ts-check
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
 export class LinkedList {
-  push() {
-    throw new Error('Remove this line and implement the function');
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._count = 0;
   }
 
+  // AJOUTER à la fin
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this._count++;
+  }
+
+  // RETIRER à la fin
   pop() {
-    throw new Error('Remove this line and implement the function');
+    if (!this.tail) return null;
+    const value = this.tail.value;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+    this._count--;
+    return value;
   }
 
+  // RETIRER au début
   shift() {
-    throw new Error('Remove this line and implement the function');
+    if (!this.head) return null;
+    const value = this.head.value;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    this._count--;
+    return value;
   }
 
-  unshift() {
-    throw new Error('Remove this line and implement the function');
+  // AJOUTER au début
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this._count++;
   }
 
-  delete() {
-    throw new Error('Remove this line and implement the function');
+  // SUPPRIMER une valeur spécifique
+  delete(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        if (current === this.head) {
+          this.shift();
+        } else if (current === this.tail) {
+          this.pop();
+        } else {
+          current.prev.next = current.next;
+          current.next.prev = current.prev;
+          this._count--;
+        }
+        return; 
+      }
+      current = current.next;
+    }
   }
 
   count() {
-    throw new Error('Remove this line and implement the function');
+    return this._count;
   }
 }
