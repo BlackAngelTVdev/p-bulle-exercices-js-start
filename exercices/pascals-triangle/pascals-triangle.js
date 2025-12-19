53e7facade41888cbf0bdca6dcc71f24a14c66c8
Damien Rochat <dami.scoot3@gmail.com>
2025-12-19 15:44:53 +0100

    Feat(/exercices/pascals-triangle/): finished

--- exercices/pascals-triangle/pascals-triangle.js
+++ exercices/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this line and implement the function');
+export const rows = (count) => {
+  const triangle = [];
+
+  for (let i = 0; i < count; i++) {
+    const row = [];
+
+    for (let j = 0; j <= i; j++) {
+
+      if (j === 0 || j === i) {
+        row.push(1);
+      } else {
+
+        const prevRow = triangle[i - 1];
+        const sum = prevRow[j - 1] + prevRow[j];
+        row.push(sum);
+      }
+    }
+
+    triangle.push(row);
+  }
+
+  return triangle;
 };
